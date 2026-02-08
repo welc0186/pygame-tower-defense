17a93ff7d9bd8bb0932f7e216fea38c0c312b3cb
Steve Welch <swelch@airgain.com>
2026-02-08 13:22:14 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+# /// script
+# dependencies = [
+#  "esper",
+#  "psygnal",
+#  "pygame",
+# ]
+# ///
+
+
+import asyncio
+import pygame
+
+# pygbag requires importing in main.py
+import esper
+
+import sys
+
+sys.path.append("./src")
+
+
+# Screen dimensions
+SCALE = 1
+WIDTH, HEIGHT = 512, 512
+
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH * SCALE, HEIGHT * SCALE))
+
+
+from pygame_tower_defense.scenes import MainScene
+
+
+from gamelib.mgmt.scene_base import SceneBase
+
+
+async def run_game(fps: int, starting_scene: SceneBase):
+    clock = pygame.time.Clock()
+
+    active_scene = starting_scene
+
+    while active_scene != None:
+        pressed_keys = pygame.key.get_pressed()
+
+        # Event filtering
+        filtered_events = []
+        for event in pygame.event.get():
+            quit_attempt = False
+            if event.type == pygame.QUIT:
+                quit_attempt = True
+            elif event.type == pygame.KEYDOWN:
+                alt_pressed = pressed_keys[pygame.K_LALT] or pressed_keys[pygame.K_RALT]
+                if event.key == pygame.K_ESCAPE:
+                    quit_attempt = True
+                elif event.key == pygame.K_F4 and alt_pressed:
+                    quit_attempt = True
+
+            if quit_attempt:
+                active_scene.terminate()
+            else:
+                filtered_events.append(event)
+
+        dt = clock.tick(fps) / 1000.0
+
+        active_scene.update(filtered_events, pressed_keys, dt)
+
+        active_scene = active_scene.next
+
+        pygame.display.flip()
+        await asyncio.sleep(0)
+
+
+pygame.display.set_caption("PyTowerDefense")
+asyncio.run(run_game(60, MainScene(screen)))
---
+++ src/pygame_tower_defense/scenes.py
@@ -0,0 +1,47 @@
+from os.path import join
+
+import esper
+from gamelib.mgmt.scene_base import SceneBase
+from gamelib.sprite.spritesheet_grid import SpriteSheetGrid
+from gamelib.ecs import (
+    RenderSurfaceProcessor,
+    RenderSurfaceComponent,
+    PositionComponent,
+)
+from gamelib.tiles.tiles import TileMap
+import pygame
+
+
+SPRITE_SHEET = SpriteSheetGrid(join("assets", "towerDefense_tilesheet.png"), 64, 64)
+GRASS_SPRITE = SPRITE_SHEET.get(3, 2)
+
+CSV_MAP = join("assets", "pygame_tower_defense.csv")
+
+RED = 255, 0, 0
+
+
+def create_backdrop() -> pygame.Surface:
+    def csv_tile_mapper(value: int) -> pygame.Surface | None:
+        return SPRITE_SHEET.get_by_index(value)
+
+    sprite_tilemap = TileMap.from_csv(CSV_MAP, mapper=csv_tile_mapper, tilesize=64)
+    return sprite_tilemap.generate_surface()
+
+
+class MainScene(SceneBase):
+    def __init__(self, screen: pygame.Surface):
+        super().__init__(screen)
+        esper.switch_world("default")
+        try:
+            esper.delete_world("main")
+        except KeyError:
+            pass
+        esper.switch_world("main")
+        esper.clear_database()
+        esper.add_processor(RenderSurfaceProcessor(screen))
+        backdrop_entity = esper.create_entity()
+        esper.add_component(backdrop_entity, PositionComponent(0, 0))
+        esper.add_component(backdrop_entity, RenderSurfaceComponent(create_backdrop()))
+
+    def update(self, events, pressed_keys, dt: float = 0) -> None:
+        esper.process(dt)
