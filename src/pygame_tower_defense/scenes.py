4340edc2fdf5ce7221312bc568f58b601fca6ab7
Steve Welch <swelch@airgain.com>
2026-02-18 20:47:37 -0600

    added a test enemy

--- src/pygame_tower_defense/scenes.py
+++ src/pygame_tower_defense/scenes.py
@@ -4,17 +4,19 @@ import esper
 from gamelib.mgmt.scene_base import SceneBase
 from gamelib.sprite.spritesheet_grid import SpriteSheetGrid
 from gamelib.ecs import (
+    MoveProcessor,
     RenderSurfaceProcessor,
     RenderSurfaceComponent,
     PositionComponent,
+    VelocityComponent,
 )
-from gamelib.tiles.tiles import TileMap
 from gamelib.tiles.tmx import TmxImageTileMap
 import pygame
 
 
 SPRITE_SHEET = SpriteSheetGrid(join("assets", "towerDefense_tilesheet.png"), 64, 64)
 GRASS_SPRITE = SPRITE_SHEET.get(3, 2)
+ENEMY_SPRITE = SPRITE_SHEET.get(15, 10)
 
 CSV_MAP = join("assets", "pygame_tower_defense.csv")
 
@@ -28,6 +30,14 @@ def create_backdrop() -> pygame.Surface:
     return sprite_tilemap.generate_surface()
 
 
+def create_enemy() -> int:
+    enemy_entity = esper.create_entity()
+    esper.add_component(enemy_entity, PositionComponent(-64, 192))
+    esper.add_component(enemy_entity, VelocityComponent((1, 0)))
+    esper.add_component(enemy_entity, RenderSurfaceComponent(ENEMY_SPRITE))
+    return enemy_entity
+
+
 class MainScene(SceneBase):
     def __init__(self, screen: pygame.Surface):
         super().__init__(screen)
@@ -38,10 +48,11 @@ class MainScene(SceneBase):
             pass
         esper.switch_world("main")
         esper.clear_database()
-        esper.add_processor(RenderSurfaceProcessor(screen))
-        backdrop_entity = esper.create_entity()
-        esper.add_component(backdrop_entity, PositionComponent(0, 0))
-        esper.add_component(backdrop_entity, RenderSurfaceComponent(create_backdrop()))
+        self.backdrop = create_backdrop()
+        esper.add_processor(MoveProcessor(), priority=50)
+        esper.add_processor(RenderSurfaceProcessor(screen), priority=10)
+        create_enemy()
 
     def update(self, events, pressed_keys, dt: float = 0) -> None:
+        self.screen.blit(self.backdrop, (0, 0))
         esper.process(dt)
